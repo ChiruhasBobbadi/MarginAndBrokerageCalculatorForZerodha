dee87f860ea4ade0094e1ef4f1227202f254486f
Chiruhas Bobbadi <chiruhas.bobbadi123@gmail.com>
2019-02-27 17:33:42 +0530

    version 1.0.1

--- app/src/main/java/com/chiruhas/android/zerodhamargincalculator/View/Activities/MainActivity.java
+++ app/src/main/java/com/chiruhas/android/zerodhamargincalculator/View/Activities/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements MarginFragment.On
 
         //add initialization
 
-        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+        MobileAds.initialize(this,"ca-app-pub-4351116683020455~8691946225");
 
     }
 
